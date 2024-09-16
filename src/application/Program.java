5b1ac4902aac16fc47740aadc64cac1c49856ff4
killa <miguel251028@gmail.com>
2024-09-16 19:42:33 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,9 @@
 package application;
 
 import model.entities.Departament;
+import model.entities.Seller;
 
+import java.util.Date;
 
 
 public class Program {
@@ -9,7 +11,10 @@ public class Program {
     public static void main(String[] args){
 
         Departament obj = new Departament(1, "books");
-        System.out.println(obj);
+
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.00, obj);
+
+        System.out.println(seller);
 
     }
 
