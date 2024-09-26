340ff2b76bf6d846531bc10cdfaba415ca83dacd
killa <miguel251028@gmail.com>
2024-09-26 20:06:55 -0300

    Reusiong instatiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,10 +2,8 @@ package application;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
-import java.util.Date;
 
 
 public class Program {
@@ -14,6 +12,7 @@ public class Program {
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
+        System.out.println("=== TEST 1: seller findById ====");
         Seller seller = sellerDao.findById(3);
 
         System.out.println(seller);
